59103b979c5b34b7c2bee09a0bad6152f0d00a74
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-04-28 23:45:07 +0630

    Tenary done

--- IfElseSwitch/IfElseSwitch/Program.cs
+++ IfElseSwitch/IfElseSwitch/Program.cs
@@ -1,7 +1,3 @@
-﻿var hour = 24;
-if (hour > 0 && hour < 12)
-    Console.WriteLine("It's morning");
-else if (hour is >= 12 and < 18)
-    Console.WriteLine("It's afternoon");
-else
-    Console.WriteLine("It's evening");
+﻿var isGoldMember = true;
+var price = isGoldMember ? 10.0f : 20.220f;
+Console.WriteLine(price);
