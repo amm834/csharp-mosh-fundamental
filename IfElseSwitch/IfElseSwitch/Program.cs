5851508e2514a14aa462adbad1039873639db446
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-04-28 23:43:55 +0630

    If else done

---
+++ IfElseSwitch/IfElseSwitch/Program.cs
@@ -0,0 +1,7 @@
+﻿var hour = 24;
+if (hour > 0 && hour < 12)
+    Console.WriteLine("It's morning");
+else if (hour is >= 12 and < 18)
+    Console.WriteLine("It's afternoon");
+else
+    Console.WriteLine("It's evening");
---
+++ ReferenceVSValue2/ReferenceVSValue2/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Person
+{
+    public int Age;
+    public static int Some;
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var x = 1;
+        Increment(x);
+        Console.WriteLine(x);
+
+        Person person = new Person()
+        {
+            Age = 50,
+        };
+        MakeOld(person);
+        Console.WriteLine(person.Age);
+    }
+
+    public static void Increment(int number)
+    {
+        number += 10;
+    }
+
+    public static void MakeOld(Person person)
+    {
+        person.Age += 20;
+    }
+}
