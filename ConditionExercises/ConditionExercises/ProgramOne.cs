3d1415c75b62627af7447d03d702347fb5300df3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-04-30 20:34:13 +0630

    Array methods done

---
+++ ConditionExercises/ConditionExercises/ProgramOne.cs
@@ -0,0 +1,30 @@
+﻿// 1- Write a program and ask the user to enter a number. The number should be between 1 to 10.
+// If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+
+while (true)
+{
+    try
+    {
+        Console.Write("Enter number between 1 to 10: ");
+
+        var input = Console.ReadLine();
+        if (String.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Invalid");
+        }
+
+        var inputNumber = int.Parse(input);
+        if (inputNumber >= 1 && inputNumber <= 10)
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Please enter only number");
+    }
+}
