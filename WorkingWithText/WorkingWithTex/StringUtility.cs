1ac2225fd70dbb712eaf24e1799cad6ee1976f41
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-01 17:37:48 +0630

    Summarizing string done

---
+++ WorkingWithText/WorkingWithTex/StringUtility.cs
@@ -0,0 +1,26 @@
+﻿namespace WorkingWithTex;
+
+public static class StringUtility
+{
+    public static string Summarize(string text, int maxLength = 20)
+    {
+        if (text.Length < maxLength)
+            return text;
+
+        var words = text.Split(' ');
+        var summaryWords = new List<string>();
+        var totalCharLength = 0;
+
+        foreach (var word in words)
+        {
+            summaryWords.Add(word);
+
+            totalCharLength += word.Length + 1;
+            if (totalCharLength > maxLength)
+                break;
+        }
+
+        var summary = string.Join(" ", summaryWords) + "...";
+        return summary;
+    }
+}
